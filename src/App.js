2338a236057e5a0850796b5e2f5fc416a447d197
harshit77 <hsahu77@gmail.com>
2020-01-31 13:37:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 function App({name}) {
 function makeRequestWithUserGesture() {
   const promise = document.requestStorageAccess();
+	console.log("cdsavc h",promise);
   promise.then(
     function () {
      alert("Yes");
