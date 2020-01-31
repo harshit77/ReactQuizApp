4cb811f7194d45463b8f60a15d34d9a9caf89386
harshit77 <hsahu77@gmail.com>
2020-01-31 13:32:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-function makeRequestWithUserGesture() {
-  const promise = document.requestStorageAccess();
-  promise.then(
-    function () {
-     alert("Yes");
-    },
-    function () {
-          alert("No");
-    }
-  );
-}
-<button onclick="makeRequestWithUserGesture()">Play video</button>
+
 
 ReactDOM.render(<App name="Stranger"/>, document.getElementById('root'));
