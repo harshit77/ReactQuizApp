75c1f8ee5c719ec2ed3a5288ad9349ffa8cf2fce
harshit77 <hsahu77@gmail.com>
2020-01-31 13:40:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function makeRequestWithUserGesture() {
 }
   return (
     <main>
-	  <button onclick={makeRequestWithUserGesture}>Play video</button>
+	  <button onClick={makeRequestWithUserGesture}>Play video</button>
 	<h1>Quiz App</h1>
 	<p>Hello {name} We have built this quiz app demo on React Hope you will like it for more info  Visit the <a href="https://reactjs.org/">React tutorial</a> to learn how to build React apps. and compare it with Svelte app <a href="https://sveltequizapp.netlify.com/">Svelte App </a> also.</p>
 	<Quiz/>
